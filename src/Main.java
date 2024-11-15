4513dfe9341dc5c6ea1a673fb13e59265dcf846c
Nancy <cookiekat987@gmail.com>
2024-11-15 20:55:53 +0800

    lesson 2, 6 update

--- src/Main.java
+++ src/Main.java
@@ -3,14 +3,43 @@
 public class Main {
 
     public static void main(String[] args) {
-        Lesson2 lesson = new Lesson2();
-        lesson.doString();
+//        //Lesson5.number3 = 3;
+//        Lesson5 lesson = new Lesson5(4);
+////        lesson.number = 1;
+//        lesson.changeNum5(1);
+//        lesson.Lesson5();
+//
+//        //int a = lesson.changeNum5(1) / 20; //没有返回值不能在公式里用
+//        double a = lesson.Method1(2);
+//        System.out.println(a);
+//        if (true && lesson.Method4())
+//            System.out.println("true");
+//        System.out.println(lesson.Method5() + " world");
+//        System.out.println(lesson.Sum(5));
+//        double b = Math.abs(3.0);
 
-        Integer original = 8;
-        Integer first = original.intValue() * 2;
-        Integer second = original.intValue() + 2;
+        Lesson6 lesson = new Lesson6();
+        lesson.doArray();
 
-        System.out.println(Math.pow(3, 2));
+        //类的方法调
+//
+//        Integer original = 8;
+//        Integer first = original.intValue() * 2;
+//        Integer second = original.intValue() + 2;
+//
+//        System.out.println(Math.pow(3, 2));
+
+        //Lesson3.doOperators();
+
+        //Lesson3.doSwitch(13);
+
+        //Lesson2 lesson = new Lesson2();
+        //lesson.doClass();
+
+        //Lesson2 lesson1 = new Lesson2();
+        //lesson1.doMath();
+
+
 
     }
 }
